3859378ff2da0a723341b44768f0abbe90de4194
Valentin Bastien <valentin.bastien@pennylane.com>
2025-05-06 16:35:31 +0200

    Revert "Update db schema"
    This reverts commit d35c0ff0f9cc430936f75ae18828b79b73c3da38.

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,16 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2021_01_11_093353) do
+ActiveRecord::Schema[6.1].define(version: 2021_01_11_093353) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
@@ -23,8 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2021_01_11_093353) do
     t.text "zip_code", null: false
     t.text "city", null: false
     t.text "country_code", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "invoice_lines", force: :cascade do |t|
@@ -36,8 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2021_01_11_093353) do
     t.text "vat_rate", null: false
     t.decimal "price", precision: 15, scale: 2, null: false
     t.decimal "tax", precision: 15, scale: 2, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["invoice_id"], name: "index_invoice_lines_on_invoice_id"
     t.index ["product_id"], name: "index_invoice_lines_on_product_id"
   end
@@ -50,8 +51,8 @@ ActiveRecord::Schema[7.0].define(version: 2021_01_11_093353) do
     t.date "deadline"
     t.decimal "total", precision: 15, scale: 2
     t.decimal "tax", precision: 15, scale: 2
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.bigint "session_id"
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
     t.index ["session_id"], name: "index_invoices_on_session_id"
@@ -63,8 +64,8 @@ ActiveRecord::Schema[7.0].define(version: 2021_01_11_093353) do
     t.text "vat_rate", null: false
     t.decimal "unit_price", precision: 15, scale: 2, null: false
     t.decimal "unit_tax", precision: 15, scale: 2, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "sessions", force: :cascade do |t|
